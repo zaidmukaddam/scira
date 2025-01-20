37590812f789af59422719d43ba4c35445e021ea
simplr-sh <simplr.sh@gmail.com>
2025-01-21 00:54:57 +0530

    Add Prettier configuration and refactor API routes for improved readability
    - Introduced a new Prettier configuration file (.prettierrc) to enforce consistent code formatting.
    - Refactored multiple API route files to enhance code readability by standardizing import statements and improving indentation.
    - Updated the `clean_images` route to streamline error handling and logging.
    - Enhanced the `trending` route by restructuring the fetch logic and improving error handling.
    - Minor adjustments in the `upload` route for better code organization and clarity.

--- app/api/clean_images/route.ts
+++ app/api/clean_images/route.ts
@@ -5,36 +5,40 @@ import { NextRequest, NextResponse } from 'next/server';
 export const runtime = 'edge';
 
 export async function GET(req: NextRequest) {
-  if (req.headers.get('Authorization') !== `Bearer ${serverEnv.CRON_SECRET}`) {
-    return new NextResponse('Unauthorized', { status: 401 });
-  }
+    if (req.headers.get('Authorization') !== `Bearer ${serverEnv.CRON_SECRET}`) {
+        return new NextResponse('Unauthorized', { status: 401 });
+    }
 
-  try {
-    await deleteAllBlobsInFolder('mplx/');
-    return new NextResponse('All images in mplx/ folder were deleted', { status: 200 });
-  } catch (error) {
-    console.error('An error occurred:', error);
-    return new NextResponse('An error occurred while deleting images', { status: 500 });
-  }
+    try {
+        await deleteAllBlobsInFolder('mplx/');
+        return new NextResponse('All images in mplx/ folder were deleted', {
+            status: 200,
+        });
+    } catch (error) {
+        console.error('An error occurred:', error);
+        return new NextResponse('An error occurred while deleting images', {
+            status: 500,
+        });
+    }
 }
 
 async function deleteAllBlobsInFolder(folderPrefix: string) {
-  let cursor;
+    let cursor;
 
-  do {
-    const listResult: ListBlobResult = await list({
-      prefix: folderPrefix,
-      cursor,
-      limit: 1000,
-    });
+    do {
+        const listResult: ListBlobResult = await list({
+            prefix: folderPrefix,
+            cursor,
+            limit: 1000,
+        });
 
-    if (listResult.blobs.length > 0) {
-      await del(listResult.blobs.map((blob) => blob.url));
-      console.log(`Deleted ${listResult.blobs.length} blobs`);
-    }
+        if (listResult.blobs.length > 0) {
+            await del(listResult.blobs.map((blob) => blob.url));
+            console.log(`Deleted ${listResult.blobs.length} blobs`);
+        }
 
-    cursor = listResult.cursor;
-  } while (cursor);
+        cursor = listResult.cursor;
+    } while (cursor);
 
-  console.log('All blobs in the specified folder were deleted');
+    console.log('All blobs in the specified folder were deleted');
 }
