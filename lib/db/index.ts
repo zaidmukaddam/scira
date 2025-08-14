a64213831754e6bdbd6f8551a7bb589514e39528
zaidmukaddam <zaidaltafmukaddam@gmail.com>
2025-08-14 11:32:34 +0530

    fix and improve yt search and bump deps

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -2,13 +2,16 @@ import { drizzle } from 'drizzle-orm/neon-http';
 import * as schema from '@/lib/db/schema';
 import { serverEnv } from '@/env/server';
 import { upstashCache } from 'drizzle-orm/cache/upstash';
+import { neon } from '@neondatabase/serverless';
 
-export const db = drizzle(serverEnv.DATABASE_URL, {
+const sql = neon(serverEnv.DATABASE_URL);
+
+export const db = drizzle(sql, {
   schema,
   cache: upstashCache({
     url: process.env.UPSTASH_REDIS_REST_URL!,
     token: process.env.UPSTASH_REDIS_REST_TOKEN!,
     global: true,
-    config: { ex: 60 },
+    config: { ex: 120 },
   }),
 });
