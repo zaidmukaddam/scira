6d8f0a37b706304a0b55f4e12e56355ea0569f50
zaidmukaddam <zaidaltafmukaddam@gmail.com>
2025-08-24 19:56:33 +0530

    Remove PostHog analytics provider

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,21 +2,12 @@
 
 import { clientEnv } from '@/env/client';
 import { ThemeProvider } from 'next-themes';
-import posthog from 'posthog-js';
-import { PostHogProvider } from 'posthog-js/react';
 import { ReactNode } from 'react';
 import React from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { TooltipProvider } from '@radix-ui/react-tooltip';
 import { UserProvider } from '@/contexts/user-context';
 import { DataStreamProvider } from '@/components/data-stream-provider';
-
-if (typeof window !== 'undefined') {
-  posthog.init(clientEnv.NEXT_PUBLIC_POSTHOG_KEY!, {
-    api_host: clientEnv.NEXT_PUBLIC_POSTHOG_HOST,
-    person_profiles: 'always',
-  });
-}
 
 // Create a client
 const queryClient = new QueryClient({
@@ -36,11 +27,9 @@ export function Providers({ children }: { children: ReactNode }) {
     <QueryClientProvider client={queryClient}>
       <UserProvider>
         <DataStreamProvider>
-          <PostHogProvider client={posthog}>
-            <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-              <TooltipProvider>{children}</TooltipProvider>
-            </ThemeProvider>
-          </PostHogProvider>
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+            <TooltipProvider>{children}</TooltipProvider>
+          </ThemeProvider>
         </DataStreamProvider>
       </UserProvider>
     </QueryClientProvider>
